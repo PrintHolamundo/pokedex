9cd78668bc2c009a53d8816d33158a555784f02a
PrintHolamundo <jdohca@gmail.com>
2022-08-05 14:29:17 -0500

    Crear pokemon en base de datos

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
+  async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toUpperCase();
-    return createPokemonDto
+
+    const createdPokemon = await this.pokemonModel.create(createPokemonDto);
+
+    return createdPokemon;
   }
 
   findAll() {
