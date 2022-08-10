6217b728d30cd5bb26c97e9f11d4aaec8c638ece
PrintHolamundo <jdohca@gmail.com>
2022-08-10 18:43:18 -0500

    configuracion de variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from '@nestjs/config';
 import { Mongoose } from 'mongoose';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
@@ -9,11 +10,12 @@ import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot('mongodb://localhost/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
 
     PokemonModule,
 
