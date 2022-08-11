eea61b9f1cd2a462fc0ab2c686e812cfba76a7d9
PrintHolamundo <jdohca@gmail.com>
2022-08-10 19:00:07 -0500

    Configuration Loader

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -16,7 +16,9 @@ export class PokemonService {
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
-  ) {}
+  ) {
+    console.log(process.env.PORT);
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase();
